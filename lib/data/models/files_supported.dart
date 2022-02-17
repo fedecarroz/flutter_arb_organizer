55023a944f46d7584d8113f2755b31deea6e96d2
Federico Carrozzino <federicocarrozzino98@gmail.com>
2022-02-17 17:37:50 +0100

    fix

--- lib/data/models/files_supported.dart
+++ lib/data/models/files_supported.dart
@@ -1,6 +1,6 @@
 class FilesSupported {
-  static String arb = 'it';
-  static String arbdoc = 'en';
+  static String arb = 'arb';
+  static String arbdoc = 'arbdoc';
   static String zip = 'zip';
 
   static List<String> get values => [arb, arbdoc, zip];
